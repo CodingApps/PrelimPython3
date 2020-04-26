39a71d6944f4d638c658acae0a6a5d172a4c953b
Rick <codingapps>
2020-04-26 00:17:00 -0700

    Updated API pulls and Jinja

--- weatherpost5.py
+++ weatherpost5.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 import os
 import json
 import time
@@ -7,26 +8,28 @@ import urllib.request
 
 app = Flask(__name__)
 
-def get_weather():
-    url = "http://api.openweathermap.org/data/2.5/forecast?id=5454711&units=imperial&appid=d1bb542ef3f0d208e77e3d35bd6f6e2a"
+def get_weather(city):
+    url = "http://api.openweathermap.org/data/2.5/forecast?q={}&units=imperial&appid=d1bb542ef3f0d208e77e3d35bd6f6e2a".format(city)
     response = urllib.request.urlopen(url).read()
     return response
 
 @app.route("/")
-@app.route("/<searchcity>")
 def index():
-    data = json.loads(get_weather())
+    searchcity = request.args.get("searchcity")
+    if not searchcity:
+        searchcity = "Philadelphia"
+    data = json.loads(get_weather(searchcity))
+    city = data['city']['name']
+    country = data['city']['country']
+    location=city
     forecast_list = []
     for d in data.get("list"):
         day = time.strftime('%d %B', time.localtime(d.get('dt')))
         mini = d.get("main").get("temp_min")
         maxi = d.get("main").get("temp_max")
-        print(day)
-        print(mini)
-        print(max)
         description = d.get("weather")[0].get("description")
         forecast_list.append((day,mini,maxi,description))
-    return render_template("index.html", forecast_list=forecast_list)
+    return render_template("index.html", forecast_list=forecast_list, location=location)
     
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
